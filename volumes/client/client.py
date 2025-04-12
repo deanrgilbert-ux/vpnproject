fbe744814f19d8c716c46ea7a9ad0cb8922ad9bf
microcoded <jacob@goodrid.ge>
2025-04-12 20:31:45 +1000

    IT WORKS!!!!!!! Split into 190B–sized blocks to enc/dec and merged after

--- volumes/client/client.py
+++ volumes/client/client.py
@@ -1,36 +1,50 @@
 #!/usr/bin/env python3
 
-import fcntl
-import struct
-import os
-import time
+import os, socket, struct, fcntl, select
 from scapy.all import *
-from shared.create_tun import createTun
-from cryptography.hazmat.primitives.asymmetric import padding
 from cryptography.hazmat.primitives import serialization, hashes
+from cryptography.hazmat.primitives.asymmetric import padding
+from shared.create_tun import createTun
 
+MAX_RSA_PLAINTEXT = 190 # 190 byte limit for RSA OEAP https://crypto.stackexchange.com/a/42100
+RSA_CIPHERTEXT_LEN = 256
+
+# Load server's public key
+with open("/keys/server_public.pem", "rb") as f:
+    public_key = serialization.load_pem_public_key(f.read())
+
+# Create the tun interface
 TUNSETIFF = 0x400454ca
 IFF_TUN   = 0x0001
 IFF_TAP   = 0x0002
 IFF_NO_PI = 0x1000
-
-# Create the tun interface
 ifname, tun = createTun(TUNSETIFF, IFF_TUN, IFF_NO_PI)
 
 os.system("ip addr add 192.168.53.99/24 dev {}".format(ifname))
 os.system("ip link set dev {} up".format(ifname))
-
 os.system("ip route add 192.168.60.0/24 dev {}".format(ifname))
- 
+
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
 # Set a default value for ip to avoid error
 ip = "10.0.0.1"
 
-### RSA ###
-# Get server public key
-with open("/keys/server_public.pem", "rb") as key_file:
-    public_key = serialization.load_pem_public_key(key_file.read())
+def split_into_blocks_encrypt(packet_bytes):
+    blocks = []
+    for i in range(0, len(packet_bytes), MAX_RSA_PLAINTEXT):
+        chunk = packet_bytes[i:i + MAX_RSA_PLAINTEXT]
+        if len(chunk) < MAX_RSA_PLAINTEXT:
+            chunk += b'\x00' * (MAX_RSA_PLAINTEXT - len(chunk))
+        encrypted = public_key.encrypt(
+            chunk,
+            padding.OAEP(
+                mgf=padding.MGF1(algorithm=hashes.SHA256()),
+                algorithm=hashes.SHA256(),
+                label=None
+            )
+        )
+        blocks.append(encrypted)
+    return b''.join(blocks)
 
 while True:
     # this will block until at least one interface is ready
@@ -40,23 +54,11 @@ while True:
             data, (ip, port) = sock.recvfrom(2048)
             pkt = IP(data)
             print("From socket <==: {} --> {}".format(pkt.src, pkt.dst))
-            os.write(tun, bytes(pkt))
+            os.write(tun, data)
 
         if fd is tun:
             packet = os.read(tun, 2048)
             pkt = IP(packet)
             print("From tun ==>: {} --> {}".format(pkt.src, pkt.dst))
-            sock.sendto(packet, ("10.9.0.11", 9090))
-
-            ### RSA ###
-            # 190 byte limit for RSA OEAP https://crypto.stackexchange.com/a/42100
-            encrypted_data = public_key.encrypt(
-                packet[:190],
-                padding.OAEP(
-                    mgf=padding.MGF1(algorithm=hashes.SHA256()),
-                    algorithm=hashes.SHA256(),
-                    label=None
-                )
-            )
+            encrypted_data = split_into_blocks_encrypt(packet)
             sock.sendto(encrypted_data, ("10.9.0.11", 9090))
-
--- volumes/server/server.py
+++ volumes/server/server.py
@@ -1,12 +1,8 @@
 #!/usr/bin/env python3
 
-import fcntl
-import struct
-import os
-
+import fcntl, struct, os
 from scapy.all import *
 from shared.create_tun import createTun
-
 from cryptography.hazmat.primitives.asymmetric import padding
 from cryptography.hazmat.primitives import serialization, hashes
 
@@ -15,6 +11,10 @@ from cryptography.hazmat.primitives import serialization, hashes
 with open("/keys/server_private.pem", "rb") as key_file:
     private_key = serialization.load_pem_private_key(key_file.read(), password=None)
 
+MAX_RSA_PLAINTEXT = 190 # 190 byte limit for RSA OEAP https://crypto.stackexchange.com/a/42100
+RSA_CIPHERTEXT_LEN = 256
+
+# Create the tun interface
 TUNSETIFF = 0x400454ca
 IFF_TUN   = 0x0001
 IFF_TAP   = 0x0002
@@ -38,6 +38,23 @@ sock.bind((IP_A, PORT))
 # Set default ip to avoid error
 ip = "10.0.0.1"
 
+def split_into_blocks_decrypt(data):
+    decrypted = b''
+    for i in range(0, len(data), RSA_CIPHERTEXT_LEN):
+        chunk = data[i:i + RSA_CIPHERTEXT_LEN]
+        if len(chunk) < RSA_CIPHERTEXT_LEN:
+            continue  # skip incomplete block
+        decrypted_block = private_key.decrypt(
+            chunk,
+            padding.OAEP(
+                mgf=padding.MGF1(algorithm=hashes.SHA256()),
+                algorithm=hashes.SHA256(),
+                label=None
+            )
+        )
+        decrypted += decrypted_block
+    return decrypted.rstrip(b'\x00')
+
 while True:
     # this will block until at least one interface is ready
     ready, _, _ = select.select([sock, tun], [], [])
@@ -45,14 +62,7 @@ while True:
         if fd is sock:
             data, (ip, port) = sock.recvfrom(2048)
             ### RSA ###
-            decrypted_data = private_key.decrypt(
-                data,
-                padding.OAEP(
-                    mgf=padding.MGF1(algorithm=hashes.SHA256()),
-                    algorithm=hashes.SHA256(),
-                    label=None
-                )
-            )
+            decrypted_data = split_into_blocks_decrypt(data)
             pkt = IP(decrypted_data)
             print("From socket <==: {} --> {}".format(pkt.src, pkt.dst))
             os.write(tun, decrypted_data)
