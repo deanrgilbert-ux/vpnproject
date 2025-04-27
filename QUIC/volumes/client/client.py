c635034b1a525f50f0bc2d88141c5988f2c05f33
Liam <liam.indiana.brown@gmail.com>
2025-04-27 09:33:53 -0400

    Changed client and server to use QUIC instead of UDP and modified code to work with an async approach

---
+++ QUIC/volumes/client/client.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python3
+
+import asyncio
+import os
+import struct
+import fcntl
+from scapy.all import *
+from aioquic.asyncio import connect
+from aioquic.quic.configuration import QuicConfiguration
+from shared.create_tun import createTun
+
+# TUN setup
+TUNSETIFF = 0x400454ca
+IFF_TUN   = 0x0001
+IFF_TAP   = 0x0002
+IFF_NO_PI = 0x1000
+
+ifname, tun = createTun(TUNSETIFF, IFF_TUN, IFF_NO_PI)
+os.system(f"ip addr add 192.168.53.99/24 dev {ifname}")
+os.system(f"ip link set dev {ifname} up")
+os.system(f"ip route add 192.168.60.0/24 dev {ifname}")
+
+async def recv_from_server(reader):
+    while True:
+        data = await reader.read(2048)
+        if not data:
+            print("Server closed connection.")
+            os._exit(1)
+        pkt = IP(data)
+        print(f"From server <==: {pkt.src} --> {pkt.dst}")
+        os.write(tun, data)
+
+def tun_read_cb(writer):
+    packet = os.read(tun, 2048)
+    pkt = IP(packet)
+    print(f"From tun ==>: {pkt.src} --> {pkt.dst}")
+    writer.write(packet)
+
+async def vpn_client():
+    configuration = QuicConfiguration(
+        is_client=True,
+        verify_mode=False
+    )
+
+    async with connect("10.9.0.11", 4433, configuration=configuration) as connection:
+        # quic = connection._quic
+        # stream_id = quic.get_next_available_stream_id()
+        reader, writer = await connection.create_stream()
+
+        loop = asyncio.get_running_loop()
+        loop.add_reader(tun, tun_read_cb, writer)
+
+        await recv_from_server(reader)  # Keep reading server
+
+if __name__ == "__main__":
+    asyncio.run(vpn_client())
---
+++ QUIC/volumes/server/server.py
@@ -0,0 +1,74 @@
+#!/usr/bin/env python3
+
+import asyncio
+import os
+import struct
+import fcntl
+from scapy.all import *
+from aioquic.asyncio import serve
+from aioquic.quic.configuration import QuicConfiguration
+from shared.create_tun import createTun
+from generate_cert import generate_self_signed_cert
+
+# TUN setup
+TUNSETIFF = 0x400454ca
+IFF_TUN   = 0x0001
+IFF_TAP   = 0x0002
+IFF_NO_PI = 0x1000
+TUN_IP = "192.168.53.98"
+SERVER_IP = "10.9.0.11"
+QUIC_PORT = 4433
+
+
+ifname, tun = createTun(TUNSETIFF, IFF_TUN, IFF_NO_PI)
+os.system(f"ip addr add {TUN_IP}/24 dev {ifname}")
+os.system(f"ip link set dev {ifname} up")
+
+class VPNServerProtocol:
+    def __init__(self, reader, writer):
+        self.reader = reader
+        self.writer = writer
+
+    async def recv_from_client(self):
+        while True:
+            data = await self.reader.read(2048)
+            if not data:
+                print("Client closed connection.")
+                os._exit(1)
+            pkt = IP(data)
+            print(f"From client <==: {pkt.src} --> {pkt.dst}")
+            os.write(tun, data)
+
+    def tun_read_cb(self):
+        packet = os.read(tun, 2048)
+        pkt = IP(packet)
+        print(f"From tun ==>: {pkt.src} --> {pkt.dst}")
+        self.writer.write(packet)
+
+    async def handle(self):
+        loop = asyncio.get_running_loop()
+        loop.add_reader(tun, self.tun_read_cb)
+        await self.recv_from_client()
+
+
+def stream_handler(reader, writer):
+    asyncio.create_task(VPNServerProtocol(reader, writer).handle())
+
+
+async def vpn_server():
+    generate_self_signed_cert()
+    configuration = QuicConfiguration(
+        is_client=False,
+    )
+    configuration.load_cert_chain(certfile="cert.pem", keyfile="key.pem")
+    server = await serve(
+        host=SERVER_IP,
+        port=QUIC_PORT,
+        configuration=configuration,
+        stream_handler=stream_handler,
+    )
+    print(f"QUIC VPN server established on IP {SERVER_IP} and port {QUIC_PORT}")
+    await asyncio.Future()
+
+if __name__ == "__main__":
+    asyncio.run(vpn_server())
