643ae5910b7b21c2dce95f3e805eb34085ba50fa
Liam <liam.indiana.brown@gmail.com>
2025-04-27 09:34:27 -0400

    Created script to generate a self signed cert in order to facilitate testing

---
+++ QUIC/volumes/server/generate_cert.py
@@ -0,0 +1,49 @@
+from cryptography import x509
+from cryptography.x509.oid import NameOID
+from cryptography.hazmat.primitives import hashes, serialization
+from cryptography.hazmat.primitives.asymmetric import rsa, ec
+from cryptography.hazmat.backends import default_backend
+import datetime
+
+def generate_self_signed_cert():
+    # Generate private key (ECDSA for faster QUIC, or RSA if you want)
+    private_key = ec.generate_private_key(ec.SECP256R1(), default_backend())
+    # private_key = rsa.generate_private_key(public_exponent=65537, key_size=2048, backend=default_backend())
+
+    # Create a self-signed cert
+    subject = issuer = x509.Name([
+        x509.NameAttribute(NameOID.COUNTRY_NAME, u"AU"),
+        x509.NameAttribute(NameOID.STATE_OR_PROVINCE_NAME, u"NSW"),
+        x509.NameAttribute(NameOID.LOCALITY_NAME, u"Sydney"),
+        x509.NameAttribute(NameOID.ORGANIZATION_NAME, u"UTS"),
+        x509.NameAttribute(NameOID.COMMON_NAME, u"10.9.0.11"),
+    ])
+
+    cert = x509.CertificateBuilder()\
+        .subject_name(subject)\
+        .issuer_name(issuer)\
+        .public_key(private_key.public_key())\
+        .serial_number(x509.random_serial_number())\
+        .not_valid_before(datetime.datetime.utcnow())\
+        .not_valid_after(datetime.datetime.utcnow() + datetime.timedelta(days=365))\
+        .add_extension(
+            x509.SubjectAlternativeName([x509.DNSName(u"10.9.0.11")]),
+            critical=False,
+        )\
+        .sign(private_key, hashes.SHA256(), default_backend())
+
+    # Serialize to PEM format (bytes)
+    cert_pem = cert.public_bytes(serialization.Encoding.PEM)
+    key_pem = private_key.private_bytes(
+        encoding=serialization.Encoding.PEM,
+        format=serialization.PrivateFormat.TraditionalOpenSSL,
+        encryption_algorithm=serialization.NoEncryption()
+    )
+
+    with open("cert.pem", "wb") as cert_file:
+        cert_file.write(cert_pem)
+
+    with open("key.pem", "wb") as key_file:
+        key_file.write(key_pem)
+
+
